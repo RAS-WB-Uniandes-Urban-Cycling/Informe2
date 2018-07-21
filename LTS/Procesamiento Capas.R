4b64f80a96a2a19aee714c0bb74a1d7b7f9547bd
Alejandro Palacio <a.palacio10@uniandes.edu.co>
2018-07-20 22:11:11 -0500

    LTS

---
+++ LTS/Procesamiento Capas.R
@@ -0,0 +1,65 @@
+#Preparación del entorno de trabajo----
+
+  #Se cargan las siguiente librerias
+    library(osmar)
+    library(osmdata)
+    library(sf)
+    library(tidyverse)
+    library(tmap)
+    library(reshape)
+    library(readxl)
+    library(googleway)
+    library(NISTunits)
+    library(units)
+    library(tmaptools)
+    library(rcompanion)
+    library(kernlab)
+    library(stplanr)
+    library(lwgeom)
+
+  #Se define las ruta de las bases de datos
+    
+    ruta_base_datos<-"/Users/alejandropalacio/Universidad de Los Andes/German Augusto Carvajal Murcia - UNIANDES - RAS - SDM/BASES DE DATOS/"
+    
+  #Se almacena los layer en cada variable definida
+    
+    malla_vial<-st_read(paste0(ruta_base_datos,"OpenStreetMaps/Malla Vial Bogotá/MVI.shp"), stringsAsFactors = FALSE)%>% filter(!(highway %in% c("cycleway","service","steps","footway"))) %>% st_transform(4326) 
+    ciclo_rutas<-st_read(paste0(ruta_base_datos,"IDECA_0318.gdb"),layer = "RBic",stringsAsFactors = FALSE) %>% st_transform(4326) 
+    zats<-st_read(paste0(ruta_base_datos,"IDECA_0318.gdb"),layer = "SCAT",stringsAsFactors = FALSE) %>% filter(SCaNombre %in% c("DOCE DE OCTUBRE","CIUDAD SALITRE NOR-ORIENTAL","CIUDAD SALITRE SUR-ORIENTAL")) %>%
+      st_transform(4326)
+    calzadas<-st_read(paste0(ruta_base_datos,"IDECA_0318.gdb"),layer = "Calz",stringsAsFactors = FALSE,promote_to_multi = FALSE) %>% st_transform(4326) %>% filter(st_is_valid(.))
+    ruta_urbana <- st_read(paste0(ruta_base_datos,"RutasSitp.gdb"),layer = "Ruta_Urbana",stringsAsFactors = FALSE) %>% st_transform(4326) 
+    
+#Procesamiento Capa Malla Vial----
+    
+  #Se hace un Join espacial entre malla vial y zats
+    
+    malla_vial<-malla_vial %>% st_join(select(zats,SCaNombre),left = FALSE, largest=TRUE)  %>% mutate(ID=row_number())%>% rename(c(SCaNombre="ZAT"))
+    
+  #Se dividen cada segmento en uno o mas segmentos
+    
+    malla_vial <-tibble(gsection(malla_vial)) %>%st_as_sf() %>% st_join(malla_vial,largest=TRUE) %>% mutate(ID=row_number()) 
+    
+  #Se hace un Join espacial entre union_malla_vial y calzadas
+    
+    malla_vial<-malla_vial %>% st_join(select(calzadas,CalAncho, CalNCarril),left = TRUE, largest=TRUE)   %>% rename(c(CalAncho="Ancho", CalNCarril="Carriles"))
+    
+  #Se obtienen las coordenadas (Lat/Long) de punto inicial de cada segmento
+    
+    coordenadas_incio_seg<-st_coordinates(malla_vial) %>% as_tibble()%>% group_by(L1)%>% filter(row_number()==1) %>% 
+      ungroup()%>%transmute(L1,Y,X)%>% rename(c(L1="ID",X="Longitud Inicio",Y="Latitud Inicio"))
+    
+    coordenadas_fin_seg<-st_coordinates(malla_vial) %>% as_tibble() %>% group_by(L1) %>% filter(row_number()==n()) %>% 
+      ungroup()%>%transmute(L1,Y,X)%>% rename(c(L1="ID",X="Longitud Fin",Y="Latitud Fin"))
+    
+  #Se hace join con la capa union_malla_vial
+    
+    malla_vial<-malla_vial  %>% left_join(coordenadas_incio_seg, By=ID) %>% left_join(coordenadas_fin_seg, By=ID)
+    
+  #Se eliminan los datos que no se usaran
+    
+    rm(coordenadas_incio_seg,coordenadas_fin_seg)
+    
+    
+    
+    
